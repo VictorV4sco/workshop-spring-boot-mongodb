f5b59938c43d12f822a1ee70fc24e76cd5b6ff44
Victor Vasco <victordcvasco@gmail.com>
2024-04-08 15:13:26 -0300

    Adding comments to posts

--- src/main/java/com/victorvasco/workshopmongo/domain/Post.java
+++ src/main/java/com/victorvasco/workshopmongo/domain/Post.java
@@ -1,25 +1,30 @@
 package com.victorvasco.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.victorvasco.workshopmongo.dto.AuthorDTO;
+import com.victorvasco.workshopmongo.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
 	}
 
@@ -63,13 +68,21 @@ public class Post implements Serializable{
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
@@ -88,4 +101,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
